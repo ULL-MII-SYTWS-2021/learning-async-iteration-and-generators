cb26b903f44b609c5d77643bb7191971e21eef5e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2020-12-21 11:29:48 +0000

    confirm

---
+++ 04-async-generators-finally/hello-async-generator-3.js
@@ -0,0 +1,19 @@
+'use strict';
+
+async function* run() {
+  await new Promise(resolve => setTimeout(resolve, 100));
+  yield 'Hello';
+  yield ('World');
+}
+
+const asyncIterator = run();
+
+// The function doesn't start running until you call `next()`
+asyncIterator.next().
+  then(hello => console.log(hello)). // { value: 'Hello', done: false }
+  then(() => { 
+    asyncIterator.next().then(world => console.log(world))
+  }).
+  then(() => { 
+    asyncIterator.next().then(world => console.log(world)) // { value: undefined, done: true }
+  });  
